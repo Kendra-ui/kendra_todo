91a1c975e74cf7c06a77c8b6078e057aba5e4d33
Kendra-ui <knokam72@gmail.com>
2023-10-27 10:11:13 +0100

    Initial commit

--- lib/utils/custom_textfield/text4.dart
+++ lib/utils/custom_textfield/text4.dart
@@ -228,186 +228,191 @@ class Text4 extends StatelessWidget {
                        ),
                      ),
       
-            SizedBox(height: MediaQuery.of(context).size.height/4,),
+            SizedBox(
+              height: MediaQuery.of(context).size.height/4,),
             Padding(
-              padding:  const EdgeInsets.only(left: 290),
-              child: FloatingActionButton(
-                backgroundColor: Colors.lightBlue,
-                 onPressed: () { 
-                  showModalBottomSheet(
-                        context: context,
-                        builder: (context) {
-                          return  SizedBox(
-                            height: 700,
-                            width: double.infinity,
-                            child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: 700,
-            decoration: BoxDecoration(
-              color:  Colors.white,
-              borderRadius: BorderRadius.circular(10)
-            ),
-            child: 
-             Column(
-              children: [
-              Padding(
-                    padding: const EdgeInsets.only(top:25),
-                    child: Container(
-                      width: MediaQuery.of(context).size.width/1.2,
-                      height: 40,
-                      decoration:  BoxDecoration(
-                        color: const Color(0xFF05243E),
-                        borderRadius: BorderRadius.circular(5)
+              padding: const EdgeInsets.only(left:250),
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height/13,
+                width: MediaQuery.of(context).size.width/7,
+                child: FloatingActionButton(
+                  backgroundColor: Colors.lightBlue,
+                   onPressed: () { 
+                    showModalBottomSheet(
+                          context: context,
+                          builder: (context) {
+                            return  SizedBox(
+                              height: 700,
+                              width: double.infinity,
+                              child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 700,
+                          decoration: BoxDecoration(
+                color:  Colors.white,
+                borderRadius: BorderRadius.circular(10)
+                          ),
+                          child: 
+                           Column(
+                children: [
+                Padding(
+                      padding: const EdgeInsets.only(top:25),
+                      child: Container(
+                        width: MediaQuery.of(context).size.width/1.2,
+                        height: 40,
+                        decoration:  BoxDecoration(
+                          color: const Color(0xFF05243E),
+                          borderRadius: BorderRadius.circular(5)
+                        ),
+                        child: 
+                        const Padding(
+                          padding:  EdgeInsets.only(top:3.0, left: 20),
+                          child: Row(
+                            children: [
+                          Icon(Icons.check_box_outlined, color: Colors.white, size: 20,),
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text('task', style: TextStyle(fontFamily: 'Poppins', fontSize: 16,letterSpacing: 1,color: Colors.white),),
+                          ),
+                            ],
+                          ),
+                        ),
                       ),
-                      child: 
-                      const Padding(
-                        padding:  EdgeInsets.only(top:3.0, left: 20),
-                        child: Row(
-                          children: [
-                        Icon(Icons.check_box_outlined, color: Colors.white, size: 20,),
-                        Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text('task', style: TextStyle(fontFamily: 'Poppins', fontSize: 16,letterSpacing: 1,color: Colors.white),),
-                        ),
-                          ],
-                        ),
-          ),
-        ),
-             ),
-            
-                 Padding(
-                  padding:  EdgeInsets.all(MediaQuery.of(context).size.width/13),
-                  child: const TextField(
-                    decoration:  InputDecoration(
-                      labelText: "Description",
-                      labelStyle: TextStyle(color: Colors.white, fontFamily: 'Poppins', letterSpacing: 1),
-                      filled: true,
-                      fillColor: Color.fromRGBO(5, 36, 62, 1),
-                      prefixIcon:  Padding(padding: EdgeInsets.only(bottom:110),
-                      child: Icon(Icons.line_weight, color: Colors.white,),) 
-                    ),
-                   style: TextStyle(fontSize: 15, color: Colors.white, fontFamily: "Poppins"),
-                  ),
-                ),
-                
-                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(width:MediaQuery.of(context).size.width/100,),
-                     SizedBox(
-                      width: MediaQuery.of(context).size.width/3,
-                      height: MediaQuery.of(context).size.height/15,
-                    child: 
-                      const TextField(
-                        decoration:  InputDecoration(
-                          labelText: "Date",
-                          labelStyle: TextStyle(color: Colors.white, fontFamily: 'Poppins', letterSpacing: 1),
-                          filled: true,
-                          fillColor: Color.fromRGBO(5, 36, 62, 1),
-                          prefixIcon:  Padding(padding: EdgeInsets.only(top:5),
-                          child: Icon(Icons.calendar_month, color: Colors.white,size: 20,),),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(5))
-                          )
+                           ),
                           
-                        ),
-                       style: TextStyle(fontSize: 15, color: Colors.white),
+                   Padding(
+                    padding:  EdgeInsets.all(MediaQuery.of(context).size.width/13),
+                    child: const TextField(
+                      decoration:  InputDecoration(
+                        labelText: "Description",
+                        labelStyle: TextStyle(color: Colors.white, fontFamily: 'Poppins', letterSpacing: 1),
+                        filled: true,
+                        fillColor: Color.fromRGBO(5, 36, 62, 1),
+                        prefixIcon:  Padding(padding: EdgeInsets.only(bottom:110),
+                        child: Icon(Icons.line_weight, color: Colors.white,),) 
                       ),
+                     style: TextStyle(fontSize: 15, color: Colors.white, fontFamily: "Poppins"),
                     ),
-                
-                     SizedBox(
-                      width: MediaQuery.of(context).size.width/3,
-                      height: MediaQuery.of(context).size.height/15,
-                    child: 
-                      const TextField(
-                        decoration:  InputDecoration(
-                          labelText: "Time",
-                          labelStyle: TextStyle(color: Colors.white, fontFamily: 'Poppins', letterSpacing: 1),
-                          filled: true,
-                          fillColor: Color.fromRGBO(5, 36, 62, 1),
-                          prefixIcon:  Padding(padding: EdgeInsets.only(top:5),
-                          child: Icon(Icons.timelapse, color: Colors.white,size: 20,),),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(5))
-                          ) 
-                        ),
-                       style: TextStyle(fontSize: 15, color: Colors.white),
-                      ),
-                    ),
-                     SizedBox(width: MediaQuery.of(context).size.width/500,)
-                  ],
-                ),
-      
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                 children: [
-
-                   SizedBox(width:  MediaQuery.of(context).size.width/1000,),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width/3,
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                        shape: MaterialStateProperty.all(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)
-                          )
-                        )
-                      ),
-                      onPressed: (){
-                        showDialog(context: context,
-                                 builder: (context)=>  AlertDialog(
-                                  title: const Text('DELETE', style: TextStyle(fontFamily: "Poppins"),),
-                                  content: const Text('Do you want to delete this task?', style: TextStyle(fontFamily: "Poppins")),
-                                  actions: <Widget>[
-                                    TextButton(onPressed: (){
-                                    Navigator.of(context).pop();
-                                    }, 
-                                    child: const Text('No',style: TextStyle(fontFamily: "Poppins")),
-                                  ),
-                                  TextButton(onPressed: (){
-                                    Navigator.of(context).pop();
-                                    }, 
-                                    child: const Text('Continue',style: TextStyle(fontFamily: "Poppins")),
-                                  )
-                                  ],
-                                 ));
-                      }, 
-                      child: const Text('cancel', style: TextStyle(fontFamily: 'Poppins', letterSpacing: 1, fontSize: 15, color: Colors.black),)),
                   ),
-                
+                  
+                   Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(width:MediaQuery.of(context).size.width/100,),
+                       SizedBox(
+                        width: MediaQuery.of(context).size.width/3,
+                        height: MediaQuery.of(context).size.height/15,
+                      child: 
+                        const TextField(
+                          decoration:  InputDecoration(
+                            labelText: "Date",
+                            labelStyle: TextStyle(color: Colors.white, fontFamily: 'Poppins', letterSpacing: 1),
+                            filled: true,
+                            fillColor: Color.fromRGBO(5, 36, 62, 1),
+                            prefixIcon:  Padding(padding: EdgeInsets.only(top:5),
+                            child: Icon(Icons.calendar_month, color: Colors.white,size: 20,),),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(5))
+                            )
+                            
+                          ),
+                         style: TextStyle(fontSize: 15, color: Colors.white),
+                        ),
+                      ),
+                  
+                       SizedBox(
+                        width: MediaQuery.of(context).size.width/3,
+                        height: MediaQuery.of(context).size.height/15,
+                      child: 
+                        const TextField(
+                          decoration:  InputDecoration(
+                            labelText: "Time",
+                            labelStyle: TextStyle(color: Colors.white, fontFamily: 'Poppins', letterSpacing: 1),
+                            filled: true,
+                            fillColor: Color.fromRGBO(5, 36, 62, 1),
+                            prefixIcon:  Padding(padding: EdgeInsets.only(top:5),
+                            child: Icon(Icons.timelapse, color: Colors.white,size: 20,),),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(5))
+                            ) 
+                          ),
+                         style: TextStyle(fontSize: 15, color: Colors.white),
+                        ),
+                      ),
+                       SizedBox(width: MediaQuery.of(context).size.width/500,)
+                    ],
+                  ),
+                    
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                   children: [
+              
+                     SizedBox(width:  MediaQuery.of(context).size.width/100,),
                     SizedBox(
                       width: MediaQuery.of(context).size.width/3,
                       child: ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(Colors.blue),
-                        shape: MaterialStateProperty.all(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)
+                        style: ButtonStyle(
+                          shape: MaterialStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)
+                            )
                           )
-                        )
-                      ),
-                      onPressed: (){
-                        
-                Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context)=>const Info()));
-      
-                      }, 
-                      child: const Text('create', style: TextStyle(fontFamily: 'Poppins', letterSpacing: 1, fontSize: 15, color: Colors.white),)),
+                        ),
+                        onPressed: (){
+                          showDialog(context: context,
+                                   builder: (context)=>  AlertDialog(
+                                    title: const Text('DELETE', style: TextStyle(fontFamily: "Poppins"),),
+                                    content: const Text('Do you want to delete this task?', style: TextStyle(fontFamily: "Poppins")),
+                                    actions: <Widget>[
+                                      TextButton(onPressed: (){
+                                      Navigator.of(context).pop();
+                                      }, 
+                                      child: const Text('No',style: TextStyle(fontFamily: "Poppins")),
+                                    ),
+                                    TextButton(onPressed: (){
+                                      Navigator.of(context).pop();
+                                      }, 
+                                      child: const Text('Continue',style: TextStyle(fontFamily: "Poppins")),
+                                    )
+                                    ],
+                                   ));
+                        }, 
+                        child: const Text('cancel', style: TextStyle(fontFamily: 'Poppins', letterSpacing: 1, fontSize: 15, color: Colors.black),)),
                     ),
-
-                    const SizedBox(width: 10,)
-                 ],
-                )
-              ],
+                  
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width/3,
+                        child: ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(Colors.blue),
+                          shape: MaterialStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)
+                            )
+                          )
+                        ),
+                        onPressed: (){
+                          
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context)=>const Info()));
+                    
+                        }, 
+                        child: const Text('create', style: TextStyle(fontFamily: 'Poppins', letterSpacing: 1, fontSize: 15, color: Colors.white),)),
+                      ),
               
-          )
-          )
-                          );
-                        });
-            
-                  }, 
-               
-                 child: const Icon(Icons.add, color: Colors.white, ),
-                ),
+                      const SizedBox(width: 10,)
+                   ],
+                  )
+                ],
+                
+                        )
+                        )
+                            );
+                          });
+                          
+                    }, 
+                 
+                   child: const Icon(Icons.add, color: Colors.white, ),
+                  ),
+              ),
             )
           ],
         ),
